62c03621a7e6eb8122eecffab782e93efe70147c
Amanraj-2937 <aman957677@gmail.com>
2026-02-05 21:41:35 +0530

    Update str(input(..).py

--- 04.Input().py/str(input(..).py
+++ 04.Input().py/str(input(..).py
@@ -1,6 +1,6 @@
-"""int(input(..))"""
+"""str(input(..))"""
 
-'''int means integers a built-in function that allows only integer to receive data from the user and run program.'''
+'''str means string a built-in function that allows only string to receive data from the user and run program.'''
 
 name = str(input("enter the name:"))
 address = str(input("enter the address:"))
