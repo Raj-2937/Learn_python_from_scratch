7f6f4ac65f4a7e2ec3485552194d0f50ef1491ca
Amanraj-2937 <aman957677@gmail.com>
2026-02-05 21:26:30 +0530

    Create input().py

---
+++ 04.Input().py/input().py
@@ -0,0 +1,11 @@
+'''Input in Python'''
+
+'''input() is a built-in function that allows a program to receive data from the user through the keyboard while the program is running'''
+
+name  = input("enter your name :")
+age   = input("enter the age :")
+marks = input("enter the marks:")
+
+print("welcome" ,name)
+print("you are young", age)
+print("your marks is good", marks)
