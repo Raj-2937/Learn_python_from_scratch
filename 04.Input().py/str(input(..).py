53d157a4ee2cb7db795516402c37e6af52d5b3f4
Amanraj-2937 <aman957677@gmail.com>
2026-02-05 21:39:54 +0530

    Create str(input(..).py

---
+++ 04.Input().py/str(input(..).py
@@ -0,0 +1,8 @@
+"""int(input(..))"""
+
+'''int means integers a built-in function that allows only integer to receive data from the user and run program.'''
+
+name = str(input("enter the name:"))
+address = str(input("enter the address:"))
+
+print(name,"you are from",address)
