b7c6ded324d45ea3e0283afcd8b33a53d2b05eb1
Amanraj-2937 <aman957677@gmail.com>
2026-02-05 21:07:35 +0530

    Create 03.Data types.py

---
+++ 03.Data types.py
@@ -0,0 +1,24 @@
+'''Data type in python.'''
+
+#Integer = +number , - number and 0.
+#Float   = Deciaml number
+#Boolean = True and False
+#None    = 
+#String  = Sentense , Words
+
+
+age = 15
+x = 67.78
+f = False 
+t = True 
+a = None
+name = "Aman Raj"
+
+
+'''we can check the type of the value using type keyword'''
+
+print("The type of name is : " , (type(name)))
+print(type(age))
+print(type(x)) 
+print(type(f))
+print(type(t))
