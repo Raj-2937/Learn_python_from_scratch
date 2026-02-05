523cc083d8ce025421618c3f8be798627aa1710f
Amanraj-2937 <aman957677@gmail.com>
2026-02-05 21:06:09 +0530

    Define variables for name and age
    Added variables for name and age with comments.

---
+++ 02.Variables.py
@@ -0,0 +1,9 @@
+"""Variable"""
+#A variable is a name given to a memory location in a program.
+
+name = "Aman Raj"
+age = 16
+
+
+'''Aman Raj and 16 is the value of the variables'''
+"""name and age is variable"""
