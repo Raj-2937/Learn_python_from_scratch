8503c7b7b208c4e127806afd744b6428b2a949a5
Amanraj-2937 <aman957677@gmail.com>
2026-02-05 22:01:10 +0530

    Create float(input( )).py

---
+++ 04.Input().py/float(input( )).py
@@ -0,0 +1,16 @@
+"""float(input(..))"""
+
+'''float means integers a built-in function that allows only floats to receive data from the user and run program.'''
+
+marks_math = float(input("enter your math (1-100) marks :"))
+marks_sci  = float(input("enter your science (1-100) marks :"))
+marks_sst  = float(input("enter your social science(1-100)  marks :"))
+marks_eng  = float(input("enter your english (1-100)  marks :"))
+
+total_marks = (marks_math + marks_sci + marks_sst + marks_eng)
+
+print("Your total marks is :", total_marks)
+
+percentage = ((total_marks / 400) * 100 )
+
+print("you percentage is ",percentage)
