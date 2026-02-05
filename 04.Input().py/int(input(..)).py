23911f4930b6eea2cb2c4c0fb63846364c9309d5
Amanraj-2937 <aman957677@gmail.com>
2026-02-05 21:35:27 +0530

    Create int(input(..)).py

---
+++ 04.Input().py/int(input(..)).py
@@ -0,0 +1,8 @@
+"""int(input(..))"""
+
+'''int means integers a built-in function that allows only integer to receive data from the user and run program.'''
+
+x = int(input("enter the number:"))
+y = int(input("enter the number:"))
+
+print(x * y)
