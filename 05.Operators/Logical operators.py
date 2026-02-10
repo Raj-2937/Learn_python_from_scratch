721fa7799cd59a5abc929bd7c588c6c340950c56
Amanraj-2937 <aman957677@gmail.com>
2026-02-10 15:42:34 +0530

    logical operator in python with examples

---
+++ 05.Operators/Logical operators.py
@@ -0,0 +1,12 @@
+"""Logical operators"""
+#( and , or , not)
+
+#example;
+
+age = int(input("Enter your age: "))
+marks = int(input("Enter your marks: "))
+
+if age >= 18 and marks >= 40:
+    print("Eligible for adimission")
+else:
+    print("Not Eligible for adimission")
