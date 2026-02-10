3f911a0cd307b568618c69b9b4d17881f769942a
Amanraj-2937 <aman957677@gmail.com>
2026-02-10 16:09:57 +0530

    if statement in python with example

---
+++ 06.Conditional_statement/1.if_statement.py
@@ -0,0 +1,10 @@
+"""If statements """
+
+#example of if statement.
+
+age = int (input("enter you age:"))
+
+if ( age  >= 18 ) : 
+    print("I can vote ") 
+if ( age <= 18 ):
+    print("I can not vote")
