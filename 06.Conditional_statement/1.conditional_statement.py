915a905491ecf8f3680d794db50579d5cf48d16b
Amanraj-2937 <aman957677@gmail.com>
2026-02-10 16:05:38 +0530

    conditional statement in python
    Correct formatting and remove unnecessary lines.

---
+++ 06.Conditional_statement/1.conditional_statement.py
@@ -0,0 +1,12 @@
+"""Conditional statement"""
+
+#Conditional Statements (Python)
+'''A conditional statement lets a program make decisions.
+It runs different code depending on whether a condition is True or False.
+In Python, we mainly use:'''
+
+# Types of conditional statement
+'''
+if
+elif
+else
