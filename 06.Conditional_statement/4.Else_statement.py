d1e16d3ea4b1ffcdc17449ce546a6e4694ff53bb
Amanraj-2937 <aman957677@gmail.com>
2026-02-10 16:17:31 +0530

    else statement with example
    Implement else statement for grading based on marks.

---
+++ 06.Conditional_statement/4.Else_statement.py
@@ -0,0 +1,14 @@
+"""else statement"""
+#else runs when all above conditions are False.
+#It does not have a condition.
+
+#example
+
+marks =int(input("enter your marks:")
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 60:
+    print("Grade B")
+else:
+    print("Grade C")
