466bc057ae4dd61a8461a9eb7e6d036eca2c3062
Amanraj-2937 <aman957677@gmail.com>
2026-02-10 16:13:38 +0530

    elif statement in python with examlpe
    Added an example demonstrating the use of elif statement in Python.

---
+++ 06.Conditional_statement/3.elif_statement.py
@@ -0,0 +1,10 @@
+"""elif statement"""
+#elif is used to check another condition if the previous if condition is False.
+
+#example
+marks = 75
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 60:
+    print("Grade B")
