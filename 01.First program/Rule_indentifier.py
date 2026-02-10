3d0512108c84b046b7b3c833046254c293b2d31b
Amanraj-2937 <aman957677@gmail.com>
2026-02-10 16:23:56 +0530

    rule_identifier

---
+++ 01.First program/Rule_indentifier.py
@@ -0,0 +1,6 @@
+"""Rules for Identifiers"""
+#1. Identifiers can be combination of uppercase and lowercase letters, digits or an underscore(_).
+#So myVariable, Aman_1, Aman_for_print all are valid python identifiers.
+#2. We can not start with digit. So while variable1 is valid, 1variable is not valid.
+#3. We can't use special symbols like !,#,@,%,$ etc in our Identifier.
+#4. Identifier can be of any length.'''
