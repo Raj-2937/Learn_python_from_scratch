6a22986fd691a1fbff0a1da629f87d93f885a85d
Amanraj-2937 <aman957677@gmail.com>
2026-02-10 15:26:02 +0530

    Relational operators with examlpes

---
+++ 05.Operators/2.Relational operator.py
@@ -0,0 +1,12 @@
+"""Relational operator"""
+#( == , != , >= , <= , < , > )
+
+#examples
+a = 50 
+b = 45
+print( a==b )
+print( a!=b ) #sigh of (!=) indicate not equal to b.
+print( a>= b)
+print( a<= b)
+print( a < b)
+print( a > b)
