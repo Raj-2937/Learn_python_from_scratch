e3356d197881a5588030daafd89eec52f95fcc10
Amanraj-2937 <aman957677@gmail.com>
2026-02-10 15:13:54 +0530

    operators and types of operator in python

---
+++ 05.Operators/operator.py
@@ -0,0 +1,9 @@
+"""Operator"""
+#A operator is symbol that performs a creation operation between operands.
+
+'''Types of operators.
+
+1.Arithmatic operators (+ , - , * , / , % ,** )
+2.Relational operators (== , <=  , >= , != ,< , >)
+3.Assigments operators (= , *= , += ,-= , %= , **=)
+4.Logical    operators ( and , or ,not )
