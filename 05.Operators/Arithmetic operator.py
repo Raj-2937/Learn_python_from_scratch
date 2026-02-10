5b0408564dcf09172769edfb159a6e1ead0372fb
Amanraj-2937 <aman957677@gmail.com>
2026-02-10 15:19:23 +0530

    Arithmetic operators with examples
    Add a Python script demonstrating arithmetic operators.

---
+++ 05.Operators/Arithmetic operator.py
@@ -0,0 +1,13 @@
+"""Arithmetic operators"""
+#(+ , - , * , % , ** , / )
+
+# 1. Arithmetice operaters.
+a = 60
+b = 50
+print(a + b)
+print(a - b)
+print(a * b)
+print(a ** b)
+print(a / b)
+print(a % b)# modulas give us remainder in 
+            #this 60%50 10 is our remainder.
