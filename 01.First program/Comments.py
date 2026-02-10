2c6c28ebe1ff31ae6f7215282351cf074bb80a06
Amanraj-2937 <aman957677@gmail.com>
2026-02-10 14:58:06 +0530

    Add comments example in Comments.py
    Add comments to demonstrate single and multi-line comment syntax.

---
+++ 01.First program/Comments.py
@@ -0,0 +1,6 @@
+# single line comments using this symbols'#'.
+"""multi line comments using this symbols thriple cot """ 
+
+'''Start where you are. Use what you have'''
+
+#you are a genius boy in the class room.
