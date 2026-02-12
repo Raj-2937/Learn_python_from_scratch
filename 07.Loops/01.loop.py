c3fd294c77c56607eb4fabdcaac8aa47ebd8396b
Amanraj-2937 <aman957677@gmail.com>
2026-02-12 08:38:33 +0530

    loops in python and types of loop.

---
+++ 07.Loops/01.loop.py
@@ -0,0 +1,7 @@
+"""Loops"""
+#Loops are used to repeat instructons.
+
+"""thier are two types of Loops in the python."""
+#for loop. = A for loop is a programming construct that allows a block of code to be ran repeatedly until a certain condition is met.
+#While loop. = A while loop is a control flow statement that repeatedly executes a block of code as long as a specified Boolean condition remains true.
+
