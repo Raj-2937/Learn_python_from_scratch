bdf11eeb5e2c8b92644baab287542119280b929b
Amanraj-2937 <aman957677@gmail.com>
2026-02-22 12:03:53 +0530

    perimeter_of_triangle

--- 04.Input().py/09.perimeter_of_triangle.py
+++ 04.Input().py/09.perimeter_of_triangle.py
@@ -1,6 +1,6 @@
-a =float(input("enter your a:    "))
-b =float(input("enter your b:    "))
-c =float(input("enter your c:    "))
+a =float(input("enter your a:  "))
+b =float(input("enter your b:  "))
+c =float(input("enter your c:  "))
 
 perimeter =(a+b+c)
 
