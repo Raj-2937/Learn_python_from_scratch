63c4cfdf187cb2afcc6e727f95e613f35d43425a
Amanraj-2937 <aman957677@gmail.com>
2026-02-22 11:22:13 +0530

    perimeter_of_parallelogram

---
+++ 04.Input().py/11.perimeter_of_parallelogram.py
@@ -0,0 +1,8 @@
+a =int(input("enter your first_adjacent angle length:"))
+b =int(input("enter your second_adjacent angle breadth:"))
+
+#we khow that perimeter of parallelogram is 2(sum of two adajecent angle)
+
+perimeter = (2*(a+b))
+
+print("perimeter of parallelgram is:",perimeter)
