ed060449ab91e1a79917043721483b1655f1ac1c
Amanraj-2937 <aman957677@gmail.com>
2026-08-11 20:48:41 +0530

    Add list initialization and print statements
    Initialize a list with mixed data types and print its elements.

---
+++ 10.List/list.py
@@ -0,0 +1,11 @@
+list = [45,6,53,346,245 ,"aman" , "aryan" ,"karan", "rohit"]
+print(list)
+
+print(type(list))
+print(list[1])
+print(list[2])
+print(list[0])
+print(list[3])
+print(list[4])
+print(list[5])
+print(list[6])
