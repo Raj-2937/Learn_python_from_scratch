857c8645db5c5df739aad5f64fb179b8144ffea3
Amanraj-2937 <aman957677@gmail.com>
2026-09-11 21:23:03 +0530

    Create function.py

---
+++ 15.Function & recursion/function.py
@@ -0,0 +1,18 @@
+# function is block of stamement that perform a specific task. 
+ 
+def calc_sub(c,d):
+    return (c - d)
+
+sum = calc_sub(23,8)
+print(sum)
+
+# two type of function built-in function,user defined function.      
+
+#bulit function
+# print() , len() , type() ,range()
+
+print("amana collage" , end="$") # sep = " "
+
+print("shrdhakhapara ") #end ="\n"
+
+#user defined function 
