b15c7129f4acd9608ab09832d33b98c178a00a54
Amanraj-2937 <aman957677@gmail.com>
2026-09-11 21:29:54 +0530

    Add files via upload

---
+++ 0pps2.py
@@ -0,0 +1,17 @@
+#`del` keyword used to delete object properties or the object itself.
+# del s1.name
+# del s1
+
+class student:
+    def __init__(self,name):
+        self.name = name
+        
+s1 = student("Aman")
+print(s1)
+del s1
+
+
+
+
+
+
---
+++ opps8.py
@@ -0,0 +1,22 @@
+# Polymorphics:
+# double dot operators overloading when the same operators are allowed to have a different meaning according to the context.
+
+# Operators and the _functions 
+# A + B has addition.          a.__add__(b)
+
+# A - B has subtraction.        a.__sub__(b)
+
+# A * B has multiplication.     a.__mul__(b)
+
+# A / B has division.           a.__turediv__(b)
+
+# A % B has addition.    a.__mod__(b)  
+
+
+
+print( 1+2) # 3 
+print(type(1))
+print("apna" + " collage")
+pritn(type("apna"))
+print([1,2,3] + [4,5,6])
+print(type([1,2,3]))
