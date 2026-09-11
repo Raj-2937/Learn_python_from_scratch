1a7b1ed0eab09a3efb2ec8d69427e569d4ba2838
Amanraj-2937 <aman957677@gmail.com>
2026-09-11 21:11:34 +0530

    Create tuples.py to demonstrate tuple usage
    Added a script to demonstrate tuple operations in Python.

---
+++ Tuples/tuples.py
@@ -0,0 +1,16 @@
+#tuple
+
+tup= (1,2,4,5,4523,7,89)
+print(tup)
+print(type(tup))
+print(tup[0:9])
+print(tup[8:9])
+print(len(tup))
+print(tup)
+print(tup[:4])
+
+if 4543 in tup:
+    print("Yes 342 is present in this tuple")
+tup2 =tup[1:4]  
+print(tup2)
+    
