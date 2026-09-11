800239d8a57f36fcd52b27719639bb8a4aea5721
Amanraj-2937 <aman957677@gmail.com>
2026-09-11 21:23:27 +0530

    Create wap.py

---
+++ 15.Function & recursion/wap.py
@@ -0,0 +1,16 @@
+
+def converter(usd_val):
+    inr_value = usd_val * 95
+    print(usd_val , "usd_val" , inr_value , "INR")
+    
+converter(232323)
+
+#5Q
+
+def check(num):
+    if (num/2==0):
+        print('even')
+    else:
+        print("odd no")
+        
+check(9)
