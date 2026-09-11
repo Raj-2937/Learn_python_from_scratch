efa1ede00137a800829b68302ee221bd0548beae
Amanraj-2937 <aman957677@gmail.com>
2026-09-11 21:16:55 +0530

    Create dictionary.py

---
+++ 13.Dictionary/dictionary.py
@@ -0,0 +1,18 @@
+dic = {
+    "Aman" : "I am Aman Raj and i am the best",
+    "sppoon" : "Object"
+    
+}
+
+print(dic["Aman"])
+
+dic2 = {
+    
+    "Gender"  : "MAN",
+    "gen"     : "Women",
+    "g"       :  "trans"
+}
+print(dic2)
+print(dic2("Gender"))
+print(dic2("gen"))
+
