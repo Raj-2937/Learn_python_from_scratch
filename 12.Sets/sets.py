45f109c1c1b3e4df53a0a6c7d5b9afaed2f53564
Amanraj-2937 <aman957677@gmail.com>
2026-09-11 21:15:13 +0530

    Create sets.py

---
+++ 12.Sets/sets.py
@@ -0,0 +1,15 @@
+s = {2,3,4,4,5,6} # sets unmutable 
+print(s)
+
+
+info = {'Carla' , 19 , False, 5.9 , 19}
+print(info)
+
+harry = { } # dictionary 
+harry2 = set() # empty sets
+print(type(harry))
+print(type(harry2))
+
+
+for value in info:
+    print(value)
